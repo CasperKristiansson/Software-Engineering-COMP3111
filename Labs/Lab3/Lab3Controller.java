2a6561b5e8c1a483ef210c1f90643a06c9369478
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-09-29 16:53:56 +0800

    Init lab3 project

---
+++ Labs/Lab3/Lab3Controller.java
@@ -0,0 +1,21 @@
+package lab3.ex2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Lab3Controller {
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Label label1;
+
+    @FXML
+    void buttonPressed(ActionEvent event) {
+
+    }
+
+}
