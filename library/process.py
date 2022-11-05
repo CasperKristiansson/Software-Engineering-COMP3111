da55581f4dde742add603594e79632f0b5cf05bd
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-11-05 19:16:05 +0800

    Simple team data class

--- library/process.py
+++ library/process.py
@@ -1,3 +1,18 @@
+class Team:
+    def __init__(self, students) -> None:
+        self.students = students
+        self.k1_energy_avg = 0
+        self.k2_energy_avg = 0
+        # self.calculate_energy_avg()
+
+    def calculate_energy_avg(self):
+        self.k1_energy_avg = sum(student.k1_energy for student in self.students) / len(self.students)
+        self.k2_energy_avg = sum(student.k2_energy for student in self.students) / len(self.students)
+
+    def __str__(self) -> str:
+        return f"Team: {self.students}"
+
+
 class Process:
     def __init__(self) -> None:
         pass
@@ -6,3 +21,5 @@ class Process:
 if __name__ == "__main__":
     p = Process()
     print(p)
+
+    team = Team(["A", "B", "C"])
