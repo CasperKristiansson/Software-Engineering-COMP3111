1cc997c0b728257534e5f1743f14483e912eb7cf
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-11-06 11:06:14 +0800

    Utilities test

---
+++ library/test_utilities.py
@@ -0,0 +1,9 @@
+import utilities
+
+
+def test_generate_data():
+    df = utilities.generate_data(100, 50, 50)
+
+    assert len(df) == 100
+    assert 45 < df['k1_energy'].mean() < 55
+    assert 45 < df['k2_energy'].mean() < 55
