20ba76d3ef115b4fc814da3a824461d6136963d3
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-11-06 10:05:13 +0800

    Add remaining student to team

--- library/process.py
+++ library/process.py
@@ -18,6 +18,24 @@ class Process:
         pass
 
 
+def add_remaining_student(df, students_df, teams):
+    min_diff = 100000
+    min_diff_index = 0
+
+    for index, row in students_df.iterrows():
+        k1_energy_avg = (sum(student['k1_energy'] for student in teams[0]) + row['k1_energy']) / (len(teams[0]) + 1)
+        k2_energy_avg = (sum(student['k2_energy'] for student in teams[0]) + row['k2_energy']) / (len(teams[0]) + 1)
+        diff = abs(k1_energy_avg - df['k1_energy'].mean()) + abs(k2_energy_avg - df['k2_energy'].mean())
+        if diff < min_diff:
+            min_diff = diff
+            min_diff_index = index
+
+    teams[0].append(students_df.loc[min_diff_index])
+    students_df.drop(min_diff_index, inplace=True)
+
+    return teams
+
+
 if __name__ == "__main__":
     p = Process()
     print(p)
