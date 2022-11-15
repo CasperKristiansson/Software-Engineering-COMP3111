6a51eaf910838dd7536feddafb0cca7f5850ce0f
NicoleWij <nicole.wijkman@gmail.com>
2022-11-15 16:23:55 +0800

    Output update
    Began working on the student inquiry service and created the base for a chart.

--- library/output.py
+++ library/output.py
@@ -1,8 +1,60 @@
+"""Contains the classes for managing the output part of the
+the program.
+"""
+__author__ = "Nicole Wijkman"
+__email__ = "nwijkman@connect.ust.hk"
+
+import pandas as pd
+pd.options.mode.chained_assignment = None
+
+
 class Output:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, teams) -> None:
+        self.teams = teams
+
+    def student_name_inquiry(self, name_input):
+        for team in self.teams:
+            for student in team.students:
+                if student["stu_name"] == name_input:
+                    return True
+        
+        return False
+
+    def student_id_inquiry(self, id_input):
+        for team in self.teams:
+            for student in team.students:
+                if student["stu_id"] == id_input:
+                    return True
+        
+        return False
+
+    def display_chart(self):
+        k1_avg = []
+        k2_avg = []
+        #k1_k2_avg = []
+
+        for team in self.teams:
+            k1_avg.append(team.k1_energy_avg)
+            k2_avg.append(team.k2_energy_avg)
+            #k1_k2_avg.append(team.k1_k2_energy_avg)
+
+        k1_avg.sort(reverse=True)
+        zipped_pairs_1 = zip(k1_avg, k2_avg)
+        #zipped_pairs_2 = zip(k1_avg, k1_k2_avg)
+        sorted_k2 = [x for _, x in sorted(zipped_pairs_1)]
+        #sorted_k1_k2 = [x for _, x in sorted(zipped_pairs_2)]
+
+        #df_chart1 = pd.DataFrame({'k1 AVG':k1_avg, 'k2 AVG':sorted_k2, 'k1 k2 AVG':sorted_k1_k2})
+        df_chart = pd.DataFrame({'k1 AVG':k1_avg, 'k2 AVG':sorted_k2})
+        return df_chart
 
 
-if __name__ == "__main__":
-    i = Output()
-    print(i)
+# if __name__ == "__main__":
+#     students = pd.read_csv(r'..\data\Sample_Student_Data_File.csv')
+
+#     p = Process(students)
+#     p.generate_teams()
+
+#     o = Output(p.teams)
+#     o.student_inquiry()
+#     o.display_chart()
--- main.py
+++ main.py
@@ -2,13 +2,45 @@ import library.process
 import library.output
 import library.input
 
+import streamlit as st
+import pandas as pd
 
 def main():
-    p = library.process.Process()
-    o = library.output.Output()
-    i = library.input.Input()
+    students = pd.read_csv(r'data\Sample_Student_Data_File.csv')
+    p = library.process.Process(students)
 
-    print(p, o, i)
+    p.generate_teams()
+
+    o = library.output.Output(p.teams)
+
+    o.display_chart()
+
+    with st.form(key='name_form', clear_on_submit=False):
+            name_input = st.text_input(label='Enter name', placeholder='SURNAME, Firstname')
+
+            submitted = st.form_submit_button("Submit")
+
+            if submitted:
+                if o.student_name_inquiry(name_input):
+                    st.write("Found")
+                else:
+                    st.write("Please try again, team not found.")
+    
+    with st.form(key='id_form', clear_on_submit=False):
+            id_input = st.text_input(label='Enter student ID', placeholder='12345678')
+
+            submitted = st.form_submit_button("Submit")
+
+            if submitted:
+                if o.student_name_inquiry(id_input):
+                    st.write("Found")
+                else:
+                    st.write("Please try again, team not found.")
+
+    
+    st.subheader('Teaming Up performance by Team Average - Graph')
+    df = o.display_chart()
+    st.line_chart(df)
 
 
 if __name__ == "__main__":
