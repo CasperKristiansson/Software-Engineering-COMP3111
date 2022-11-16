d946c4a74443dd289c797dc5f6244cafaadec5df
MichellDib <michell.dib28@gmail.com>
2022-11-16 15:12:09 +0800

    update

--- library/input.py
+++ library/input.py
@@ -19,9 +19,6 @@ class Input:
         self.df = pd.read_csv(filename)
         for _, row in self.df.iterrows():
             self.students.append(Student(row['stu_id'], row['stu_name'], row['email'], row['k1_energy'], row['k2_energy'], row['k3_tick1'], row['k3_tick2'], row['my_pref'], row['concerns']))
-        self.students = pd.DataFrame([vars(student) for student in self.students])
-            
-            
 
 
 class Student:
