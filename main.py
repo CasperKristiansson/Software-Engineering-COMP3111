f60ad9fcfcdd4716551073589137cc3d80de5168
Casper Kristiansson <casper.kristiansson@yahoo.se>
2022-11-21 13:55:34 +0800

    Main refactoring

--- main.py
+++ main.py
@@ -23,21 +23,20 @@ def main():
 
     st.markdown("<h2 style='text-align: center'>Output</h2>", unsafe_allow_html=True)
 
-    _, button_column, _ = st.columns([41, 8, 40])
+    _, button_column, _ = st.columns([41, 9, 40])
     with button_column:
         button = st.button("Generate Teams", disabled=not uploaded_file)
         find_team_button = st.button("Find your team", disabled=not button)
 
-        if st.session_state.get('button') != True:
-            st.session_state['button'] = button # Saved the state of button
+        if not st.session_state.get('button'):
+            st.session_state['button'] = button  # Saved the state of button
 
-        if st.session_state.get('find_team_button') != True:
-            st.session_state['find_team_button'] = find_team_button # Saved the state find_team_button
-
+        if not st.session_state.get('find_team_button'):
+            st.session_state['find_team_button'] = find_team_button  # Saved the state find_team_button
 
     _, chart_column, _ = st.columns([2, 5, 2])
     with chart_column:
-        if st.session_state['button'] == True:
+        if st.session_state['button']:
             p = library.process.Process(i.students)
 
             with st.spinner("Generating Teams..."):
@@ -49,14 +48,12 @@ def main():
             st.subheader('Teaming Up performance by Team Average - Graph')
             st.line_chart(df)
 
-            if st.session_state['find_team_button'] == True:
+            if st.session_state['find_team_button']:
                 st.subheader('Find your team: ')
-                
+
                 with st.form(key='name_form', clear_on_submit=False):
                     name_input = st.text_input(label='Enter name', placeholder='SURNAME, Firstname')
-                    submitted = st.form_submit_button("Submit")
-
-                    if submitted:
+                    if st.form_submit_button("Submit"):
                         df = o.student_name_inquiry(name_input)
 
                         if df is None:
@@ -66,9 +63,7 @@ def main():
 
                 with st.form(key='id_form', clear_on_submit=False):
                     id_input = st.text_input(label='Enter student ID', placeholder='12345678')
-                    submitted = st.form_submit_button("Submit")
-
-                    if submitted:
+                    if st.form_submit_button("Submit"):
                         df = o.student_id_inquiry(id_input)
 
                         if df is None:
